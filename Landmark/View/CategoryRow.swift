91f918a58a6d966883af40a8edd0d102e9296a57
Shashwat Kashyap <shashwat.kashyap@icloud.com>
2023-11-30 17:04:01 +0530

    Added Category And Featured

---
+++ Landmark/View/CategoryRow.swift
@@ -0,0 +1,40 @@
+//
+//  CategoryRow.swift
+//  Landmark
+//
+//  Created by Shashwat Kashyap on 30/11/23.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var categoryName: String
+    var items: [Landmark]
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(categoryName)
+                .font(.headline)
+                .padding(.leading, 15)
+                .padding(.top, 5)
+
+
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .top, spacing: 0) {
+                    ForEach(items) { landmark in
+                        CategoryItem(landmark: landmark)
+                    }
+                }
+            }
+            .frame(height: 185)
+        }
+    }
+}
+
+#Preview {
+    let landmarks = ModelData().landmarks
+    return CategoryRow(
+        categoryName: landmarks[0].category.rawValue,
+        items: Array(landmarks.prefix(3))
+    )
+}
